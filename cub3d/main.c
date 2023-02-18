0ad0e7045eaaf45bdaa6e3b0b00e89bbd4026643
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-02-18 21:05:44 +0100

    Add files via upload

---
+++ cub3d/main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tboumadj <tboumadj@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 00:19:45 by tboumadj@student  #+#    #+#             */
+/*   Updated: 2023/02/16 16:10:02 by tboumadj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube.h"
+
+int	main(int argc, char **argv)
+{
+	t_map	map;
+
+	if (argc != 2)
+	{
+		printf("Bad number of arguments !\n");
+		exit(1);
+	}
+	init(&map, argv);
+	//---------------PRINT----------------
+	/*int i = 0;
+	while (map.map[i])
+		printf("map= %s", map.map[i++]);
+	printf("\n");
+	printf("\n");
+	printf("posx = %d ", map.playX);
+	printf("posy = %d\n", map.playY);
+	printf("\n");
+	i = 0;
+	while (i < 7)
+		printf("%s", map.text[i++]);
+	printf("\n");
+	//-----------------------------------*/
+	test_loop(&map);
+	mlx_hook(map.win, 2, 1L << 0, action_ray_game, &map);
+	mlx_hook(map.win, 17, 1L << 17, ft_close, &map);
+	mlx_loop(map.mlx);
+	//free_exit(&map);
+	return (0);
+}
