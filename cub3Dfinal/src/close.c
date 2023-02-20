37c41840ede0a4ec31fbc59f1d6129b44fb06112
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-02-21 00:08:50 +0100

    Add files via upload

--- cub3Dfinal/src/close.c
+++ cub3Dfinal/src/close.c
@@ -6,7 +6,7 @@
 /*   By: tboumadj <tboumadj@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 16:00:05 by tboumadj          #+#    #+#             */
-/*   Updated: 2023/02/20 23:49:58 by tboumadj         ###   ########.fr       */
+/*   Updated: 2023/02/21 00:05:07 by tboumadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@ int	ft_close(t_map *map)
 	mlx_destroy_window(map->mlx, map->win);
 	if (map->img)
 		mlx_destroy_image(map->mlx, map->img);
+	if (map->texture.nord)
+		free(map->texture.nord);
+	if (map->texture.sud)
+		free(map->texture.sud);
+	if (map->texture.ouest)
+		free(map->texture.ouest);
+	if (map->texture.est)
+		free(map->texture.est);
 	free_exit(map);
 	exit(EXIT_SUCCESS);
 	return (0);
